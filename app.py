c0691c3bdd5148951f2582fe2c152ccaaeff2788
sailom124 <6531501124@lamduan.mfu.ac.th>
2025-04-20 16:50:36 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Created on Sun Apr 20 16:14:09 2025
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+
 
 with open('kmeans_model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
@@ -17,6 +17,7 @@ st.title("K-Means Clustering Visualizer by Sailom")
 
 st.set_page_config(page_title = "K-Means Clustering", layout = "centered")
 
+from sklearn.datasets import make_blobs
 X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
 
 y_kmeans = loaded_model.predict(X)
